5218f026d472d0e02f4609ca782a891f6c5f0912
Ollie.Lin <Ollie.Lin@skylinetw.com>
2019-02-16 15:20:15 +0800

    init project

---
+++ Tdd.AccountingPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tdd.AccountingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
