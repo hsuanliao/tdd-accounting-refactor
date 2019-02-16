b4d415661a8682d60d18f65d693a79f11b9e10e0
Joey Chen <jay.hatelove@gmail.com>
2019-02-16 20:51:32 +0800

    move Period to independent file

--- Tdd.AccountingPractice/Accounting.cs
+++ Tdd.AccountingPractice/Accounting.cs
@@ -4,23 +4,6 @@ using System.Linq;
 
 namespace Tdd.AccountingPractice
 {
-    public class Period
-    {
-        public Period(DateTime start, DateTime end)
-        {
-            Start = start;
-            End = end;
-        }
-
-        public DateTime Start { get; private set; }
-        public DateTime End { get; private set; }
-
-        public int EffectiveDays()
-        {
-            return (End - Start).Days + 1;
-        }
-    }
-
     public class Accounting
     {
         private IBudgetRepo budgetRepo;
