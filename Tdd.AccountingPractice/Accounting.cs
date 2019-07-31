2922113b444b836188a06092831ad52de90495d2
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-07-31 23:11:07 +0800

    remove unless code

--- Tdd.AccountingPractice/Accounting.cs
+++ Tdd.AccountingPractice/Accounting.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Tdd.AccountingPractice
 {
     public class Accounting
     {
-        private IBudgetRepo budgetRepo;
+        private readonly IBudgetRepo budgetRepo;
 
         public Accounting(IBudgetRepo repo)
         {
@@ -25,39 +23,8 @@ namespace Tdd.AccountingPractice
             {
                 totalAmount += budget.OverlappingAmount(period);
             }
-            //var monthsInTargetRange = GetMonthsInTargetRange(start, end);
-            //for (int i = 0; i <= monthsInTargetRange; i++)
-            //{
-            //    var currentDate = start.AddMonths(i);
-            //    var budget = GetBudget(budgetList, currentDate);
-            //    if (budget != null)
-            //    {
-            //        totalAmount += budget.OverlappingAmount(period);
-            //    }
-            //}
 
             return totalAmount;
-        }
-
-        private Budget GetBudget(IEnumerable<Budget> budgets, DateTime target)
-        {
-            return budgets.FirstOrDefault(x => x.YearMonth == target.ToString("yyyyMM"));
-        }
-
-        private int GetDayInTargetMonth(DateTime targetDateTime)
-        {
-            return DateTime.DaysInMonth(targetDateTime.Year, targetDateTime.Month);
-        }
-
-        private int GetMonthsInTargetRange(DateTime start, DateTime end)
-        {
-            var diffMonths = 12 * (end.Year - start.Year) + (end.Month - start.Month);
-            return diffMonths;
-        }
-
-        private bool IsSameMonth(DateTime start, DateTime end)
-        {
-            return start.ToString("yyyyMM") == end.ToString("yyyyMM");
         }
 
         private bool IsValidateDateRange(DateTime start, DateTime end)
